1eb5dc48a39a29a7b9705c93e2d75b8c9124ddb5
ITHanan <155847905+ITHanan@users.noreply.github.com>
2024-11-22 13:52:23 +0100

    Create some styling for the code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
 ﻿using AdventureGame;
+using Spectre.Console;
 using System.Text.Json;
+
+using AdventureGame;
+using Spectre.Console;
+using System.Text.Json;
+using Figgle;
 
 public class Program
 {
@@ -13,52 +19,47 @@ public class Program
         string dataJsonFilePath = "AdventureGameData.json";
         string allDataAsJson = File.ReadAllText(dataJsonFilePath);
         MyDB myDB = JsonSerializer.Deserialize<MyDB>(allDataAsJson)!;
-        var me = new Program();
 
+
+        var welcomeMessage = FiggleFonts.Standard.Render("Welcome to the Game!");
+
+        // Display the message with Spectre styling
+        AnsiConsole.MarkupLine($"[bold red]{welcomeMessage}[/]");
 
         while (true)
         {
-            // Display menu to the user
-            Console.WriteLine("\n1. Add Player");
-            Console.WriteLine("2. Remove Player");
-            Console.WriteLine("3. Update Player");
-            Console.WriteLine("4. Show All Players");
-            Console.WriteLine("5. Show Player by ID");
-            Console.WriteLine("6. Play As Player");
-            Console.WriteLine("7. Exit");
+            // Display menu to the user and get the selected choice
+            string choice = displayMenu();
 
-            // Get user choice
-            string choice = Console.ReadLine()!;
-
+            // Handle user choice
             switch (choice)
             {
-                case "1":
+                case "1. Add Player":
                     adventurGameSystem.AddPlayer(myDB);
                     break;
 
-                case "2":
+                case "2. Remove Player":
                     adventurGameSystem.RemovePlayer(myDB);
                     break;
 
-                case "3":
+                case "3. Update Player":
                     adventurGameSystem.UpdatePlayer(myDB);
                     break;
 
-                case "4":
-                    adventurGameSystem.ShowAllPlayers(myDB);
+                case "4. Show All Players":
+                    adventurGameSystem.ViewPlayers(myDB);
                     break;
 
-                case "5":
-                    adventurGameSystem.ShowPlayerById(myDB);
+                case "5. Show Player by ID":
+                    adventurGameSystem.ShowAPlayerById(myDB);
                     break;
 
-
-                case "6":
-                    PlayAsPlayer(myDB);
+                case "6. Play As Player":
+                    adventurGameSystem.PlayAsPlayer(myDB);
                     break;
 
-                case "7":
-                    return;
+                case "7. Exit":
+                    return; // Exit the program
 
                 default:
                     Console.WriteLine("Invalid choice. Please try again.");
@@ -67,95 +68,24 @@ public class Program
         }
     }
 
-
-    public static void PlayAsPlayer(MyDB myDB) 
+    private static string displayMenu()
     {
-        AdventurGameSystem adventurGameSystem = new AdventurGameSystem();
-
-        Console.WriteLine("Enter Player ID:");
-       int Id = Convert.ToInt32( Console.ReadLine());
-        var playerManager = new GeneriskaClass<Player>();
-        foreach (var p in myDB.AllPlayersDatafromAdventureGameData)
-        {
-
-            playerManager.AddTo(p);
-
-        }
-
-        Player player = playerManager.GetByID(Id);
-        if (player == null) 
-        {
-
-            Console.WriteLine("Player not found.");
-            return;
-        }
-
-        Console.WriteLine($"You are now playing as {player.Name}!");
-
-        while (true)
-        {
-            Console.WriteLine("\n--- Player Actions ---");
-            Console.WriteLine("1. Attack Another Player");
-            Console.WriteLine("2. Take Damage (simulate)");
-            Console.WriteLine("3. Add a Weapon");
-            Console.WriteLine("4. Collect Coins");
-            Console.WriteLine("5. View Player Stats");
-            Console.WriteLine("6. Exit Player Mode");
-            Console.WriteLine("Choose an option:");
-
-            string action = Console.ReadLine()!;
-
-            switch (action)
-            {
-                case "1": // Attack another player
-                    Console.WriteLine("Enter the target Player ID to attack:");
-                    int targetId = int.Parse(Console.ReadLine()!);
-                    var targetPlayer = playerManager.GetByID(targetId);
-
-                    if (targetPlayer != null)
-                    {
-                        player.Attack(targetPlayer);
-                        adventurGameSystem.SaveAllData(myDB);
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("Target player not found.");
-                    }
-                    break;
-
-                case "2": // Simulate taking damage
-                    Console.WriteLine("Enter the amount of damage:");
-                    int damage = int.Parse(Console.ReadLine()!);
-                    player.TakeDamage(damage);
-                    break;
-
-                case "3": // Add a weapon
-                    Console.WriteLine("Enter the name of the weapon to add:");
-                    string weapon = Console.ReadLine()!;
-                    player.AddWeapon(weapon);
-                    break;
-
-                case "4": // Collect coins
-                    Console.WriteLine("Enter the number of coins to collect:");
-                    int coins = int.Parse(Console.ReadLine()!);
-                    player.CollectCoins(coins);
-                    break;
-
-                case "5": // Display stats
-                    player.DisplayStats();
-                    break;
-
-                case "6": // Exit player mode
-                    Console.WriteLine($"Exiting player mode for {player.Name}.");
-                    return;
-
-                default:
-                    Console.WriteLine("Invalid option. Please try again.");
-                    break;
-            }
-        }
-
+        // Display a menu using Spectre.Console's SelectionPrompt
+        return AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("[green]Select an option from the Player Management Menu:[/]")  // Title of the menu
+                .PageSize(6)  // Number of items to show at once
+                .MoreChoicesText("[grey](Move up and down to reveal more options)[/]")  // Text when more options exist
+                .AddChoices(new[]  // Add the menu options
+                {
+                    "1. Add Player",
+                    "2. Remove Player",
+                    "3. Update Player",
+                    "4. Show All Players",
+                    "5. Show Player by ID",
+                    "6. Play As Player",
+                    "7. Exit"
+                })
+        );
     }
-
 }
